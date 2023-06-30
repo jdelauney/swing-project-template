44f38f620d37d51493ba7b7246a47430c963d19f
jdelauney <delauney.jerome@gmail.com>
2023-06-30 21:54:45 +0200

    chore: Init project
    - add & config vite
    - add & config postcss
    - add & config lightningcss
    - add & config eslint
    - add & config prettier
    - add & config stylelint
    - add lint-staged & husky
    - add boilerplate html/css/js

---
+++ postcss-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	syntax: "postcss-scss",
+	map: false,
+	plugins: [
+		/* fix flex bugs : cf https://github.com/philipwalton/flexbugs */
+		require('postcss-flexbugs-fixes')({ bug6: false }),
+		require('postcss-sort-media-queries')({
+			sort: 'mobile-first',
+			configuration: {
+				unitlessMqAlwaysFirst: true,
+			}
+		}),
+	]
+};
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+// vite.config.ts
+import type, { UserConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
+import lightningcss from 'vite-plugin-lightningcss';
+
+export default defineConfig({
+  plugins: [
+    lightningcss({
+      browserslist: '>= 0.25%',
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        require('postcss-flexbugs-fixes')({
+          bug6: false
+        }),
+        require('postcss-sort-media-queries')({
+          sort: 'mobile-first',
+          configuration: {
+            unitlessMqAlwaysFirst: true,
+          }
+        }),
+      ]
+    },
+  }
+});
